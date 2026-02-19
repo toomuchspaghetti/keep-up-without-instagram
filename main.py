51b9139cfcdc382ab335615021ccb431cc333e89
toomuchspaghetti <a@a.a>
2026-02-19 15:02:23 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,73 @@
+from dotenv import load_dotenv
+from typing import Final
+import os
+from telegram.ext import ApplicationBuilder, ExtBot
+import asyncio
+from time import sleep
+from datetime import datetime
+import subprocess
+from PIL import Image, ImageChops
+import io
+import re
+
+SLEEP_SECONDS: Final = 5 * 60
+WORK_DIR: Final = "work"
+OLD_IMAGE_FILENAME: Final = "old.png"
+NEW_IMAGE_FILENAME: Final = "new.png"
+
+def send_notification(bot: ExtBot, chat_id: int):
+    asyncio.run(bot.send_message(chat_id, "you have a notification from instagram!"))
+
+def load_image(filename: str):
+    try:
+        return Image.open(os.path.join(WORK_DIR, filename))
+    except Exception:
+        return
+
+def main():
+    try:
+        if not load_dotenv(override=True):
+            raise Exception()
+
+        TELEGRAM_TOKEN: Final[str] = os.environ["TELEGRAM_TOKEN"]
+        TELEGRAM_CHAT_ID: Final = int(os.environ["TELEGRAM_CHAT_ID"])
+    except Exception:
+        print("cannot load env")
+        exit(1)
+
+    def send_notification_quick(bot: ExtBot):
+        send_notification(bot, TELEGRAM_CHAT_ID)
+
+    os.makedirs(WORK_DIR, exist_ok=True)
+
+    while True:
+        try:
+            print(f"trying... {datetime.now().astimezone().isoformat()}")
+
+            result = re.search(r"success", subprocess.run(["node", "main.ts"], capture_output=True).stdout.decode())
+
+            if result == None:
+                raise Exception("cannot find \"success\", node script failed")
+
+            old_image = load_image(OLD_IMAGE_FILENAME)
+            
+            if old_image != None:
+                new_image = load_image(NEW_IMAGE_FILENAME)
+
+                if new_image == None:
+                    raise Exception("cannot find new image, node script is buggy!")
+                
+                difference_image = ImageChops.difference(old_image, new_image)
+
+                if difference_image.getbbox() != None:
+                    send_notification_quick(ApplicationBuilder().token(TELEGRAM_TOKEN).build().bot)
+
+            os.rename(os.path.join(WORK_DIR, NEW_IMAGE_FILENAME), os.path.join(WORK_DIR, OLD_IMAGE_FILENAME))
+        except Exception as e:
+            print(f"whoops! error: {e}")
+            raise
+
+        sleep(SLEEP_SECONDS)
+
+if __name__ == "__main__":
+    main()
---
+++ main.ts
@@ -0,0 +1,72 @@
+import puppeteer, { ElementHandle, JSHandle } from 'puppeteer-core';
+import 'dotenv/config'
+import { setTimeout } from 'node:timers/promises'
+
+let instagram_session_id;
+
+try {
+    instagram_session_id = process.env["INSTAGRAM_SESSION_ID"]
+
+    if (instagram_session_id === undefined) {
+        throw Error()
+    }
+} catch {
+    console.error("cannot load env")
+    process.exit(1)
+}
+
+const browser = await puppeteer.launch({
+    executablePath: "/usr/bin/chromium",
+});
+
+try {
+    await browser.setCookie(
+      {
+        name: 'sessionid',
+        value: instagram_session_id,
+        domain: '.instagram.com',
+        path: '/',
+        expires: -1,
+        httpOnly: true,
+        secure: true,
+    });
+    const page = await browser.newPage();
+    await page.setViewport({width: 1920, height: 1080});
+    await page.goto("https://www.instagram.com/direct/inbox/");
+
+    await page.waitForFunction(() => {
+        const crappy_bar = document.body.children[6].children[0].children[0].children[1].children[0].children[0].children[0].children[0].children[0].children[0].children[0].children[0]
+        
+        if (crappy_bar === undefined) return;
+        
+        crappy_bar.remove();
+        return true;
+    })
+    await page.waitForFunction(() => {
+        const button = document.getElementsByTagName("button")[2]
+        
+        if (button === undefined || button.textContent.search(/Not Now/) < 0) {
+            return;
+        }
+        
+        button.click();
+        return true;
+    })
+    const handle = await page.waitForFunction(() => document.getElementsByTagName("h1")[0].parentElement.parentElement.children[2])
+
+    
+    await handle.screenshot({
+        path: "work/new.png",
+        clip: {
+            x: 87,
+            y: 0,
+            width: 13,
+            height: 10000,
+        },
+        captureBeyondViewport: false
+    });
+
+    console.log("success")
+} finally {
+    await browser.close()
+}
